c9190fa30f0ebc9108d19f7b96a76bf9e532dec0
geospaces-org <geo1@geospaces.org>
2024-07-04 15:10:02 -0600

    Commiting

--- dash_services.py
+++ dash_services.py
@@ -4,6 +4,8 @@
 
 import sys, os,  glob, logging, datetime
 from  mangorest.mango import webapi
+from colabexts import utils as colabexts_utils
+import pandas as pd
 
 sys.path.append("../..")
 
@@ -68,15 +70,78 @@ def testdash( t=2, **kwargs):
     time.sleep( int(t) )
     return f"Slept for {t} seconds :)"
 #------------------------------------------------------------------------------
+def _getSystemStats(request=None, **kwargs):
+    import platform,  socket, psutil,  random
+
+    kb,mb, gb = float(1024), float(1024 * 1024), float( 1024 * 1024 * 1024)
+
+    memTotal       = int(psutil.virtual_memory()[0]/gb)
+    memFreeGB        = int(psutil.virtual_memory()[1]/gb)
+    memUsedGB        = int(psutil.virtual_memory()[3]/gb)
+    memFreeMB        = int(psutil.virtual_memory()[1]/mb)
+    memUsedMB        = int(psutil.virtual_memory()[3]/mb)
+    memPercent     = int(memUsedGB/memTotal*100)
+    storageTotal   = int(psutil.disk_usage('/')[0]/gb)
+    storageUsed    = int(psutil.disk_usage('/')[1]/gb)
+    storageFree    = int(psutil.disk_usage('/')[2]/gb)
+    storagePercent = int(storageUsed/storageTotal*100)
+    info           = "CPU"
+    ts             = int(datetime.datetime.now().timestamp()*1000) + 1000
+
+    out=f''' {{
+        time            : {ts},
+        cpu_count       : {os.cpu_count()},
+        host            : "{socket.gethostname()}",
+        System          : "{platform.system(),platform.machine()}",
+        memoryUnits     : "GiB",
+        memory          : {memTotal}, 
+        memFreeGB       : {memFreeGB},
+        memUsedGB       : {memUsedGB},
+        memFreeMB       : {memFreeMB},
+        memUsedMB       : {memUsedMB},
+        memory_prcnt    : {memPercent},
+        n_processes     : {len(psutil.pids())},
+        load_avg_1min   : {round(os.getloadavg()[0],2)},
+        load_avg_5min   : {round(os.getloadavg()[1],2)},
+        load_avg_15min  : {round(os.getloadavg()[2],2)},
+        lat             : {   39.5199452 + random.random() } ,
+        lon             : { -104.9390802 + random.random() } ,
+        altitude        : { 10000 + random.random() * 10   } ,  # altitude in in feet
+        temperature     : {random.random() * 10 + 32}
+
+    }}'''
+    p = colabexts_utils.parsej(out, **kwargs)
+    df = pd.DataFrame([p])
+    return df
+#------------------------------------------------------------------------------
+SYSTEM_INFO_DF = None
+@webapi("/dashboard/system_info")
+def system_info( request, **kwargs):
+    global SYSTEM_INFO_DF
+    df = _getSystemStats(request, **kwargs)
+    if (SYSTEM_INFO_DF is None):
+        SYSTEM_INFO_DF = df
+    else:
+        SYSTEM_INFO_DF = pd.concat([df, SYSTEM_INFO_DF])
+
+    SYSTEM_INFO_DF = SYSTEM_INFO_DF[-500:] # just keep last 500 rows for debugging
+    ret = {
+        "name" : "system_info",
+        'columns': [c for c in SYSTEM_INFO_DF.columns],
+        'values' : SYSTEM_INFO_DF.values.tolist()        
+    }
+    return ret
+
+#------------------------------------------------------------------------------
 @webapi("/dashboard/sample_data")
-def testdash( request, **kwargs):
+def sample_data( request, **kwargs):
     from random import random 
     n = kwargs.get("n", 1)
     
     ts = int(datetime.datetime.now().timestamp()*1000) 
     ret = {
         "name": "sample_data",
-        "columns": ["time"] + [ "v{i}" for i in range(n)],
+        "columns": ["time"] + [ f"v{i}" for i in range(n)],
         "values":  [[ts+i*1000] + [random() for j in range(n)] for i in range(100)]
     }
     return ret
